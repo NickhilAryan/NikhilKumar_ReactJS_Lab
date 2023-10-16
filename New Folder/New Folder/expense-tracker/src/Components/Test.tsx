40eb7652ab4802ad02d3b9faf0a053ade198cb55
NickhilAryan <114022421+NickhilAryan@users.noreply.github.com>
2023-10-16 14:37:12 +0530

    Add files via upload

---
+++ New Folder/New Folder/expense-tracker/src/Components/Test.tsx
@@ -0,0 +1,14 @@
+function Test() {
+    const sta = ["dfv", "sfdv", "rtg"];
+    return (
+        <>
+            {sta && (
+                sta.map((ab) => (
+                    <div>{ab}</div>
+                ))
+            )}
+        </>
+    );
+}
+
+export default Test;
